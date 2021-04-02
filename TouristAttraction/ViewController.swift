51dadd66951b0e332465da12ca8609fe444ce7f8
Mohit Sharma <>
2021-04-03 00:52:53 +0530

    Empty Project Setup

---
+++ TouristAttraction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouristAttraction
+//
+//  Created by Mohit Sharma on 02/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
